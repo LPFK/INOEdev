c416efb3eff56db4ea76ccaa6a51e08f86ed186d
unknown <liliankozlowski36@gmail.com>
2021-09-27 13:23:40 +0200

    Ajout du scss, des fonts, du js

--- assets/app.js
+++ assets/app.js
@@ -11,5 +11,6 @@ import './styles/app.scss';
 //You can specify which plugins you need
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
+import './js/theme'
 // start the Stimulus application
 import './bootstrap';
